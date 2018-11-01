ce9044a24243cac86ed6087db9c0ca989a4268b6
olsamat <4x74av@gmail.com>
2018-11-01 09:38:13 +0200

    second commit second Foo, но есть варианты сбора

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StringFromLettersCompare
@@ -13,7 +15,46 @@ namespace StringFromLettersCompare
             string text = "attachet";
             //string[] some = { "r>i", "n>g", "i>n", "s>t", "t>r" };
             string[] some = { "e>t", "t>t", "a>t", "h>e", "c>h", "a>c", "t>a" };
-            Console.WriteLine(StringFromLetters(some));
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine(Foo(some));
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedTicks.ToString());
+            //stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            //Console.WriteLine(StringFromLetters(some));
+            //stopwatch.Stop();
+            //Console.WriteLine(stopwatch.ElapsedTicks.ToString());
+
+        }
+        static string Foo(string[] str)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            HashSet<int> table = new HashSet<int>();
+            stringBuilder.Append(str[0][0]);
+            stringBuilder.Append(str[0][2]);
+            table.Add(0);
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = i; j < str.Length; j++)
+                {
+                    if (!table.Contains(j))
+                    {
+                        if (stringBuilder[0] == str[j][2])
+                        {
+                            stringBuilder.Insert(0, str[j][0]);
+                            table.Add(j);
+                        }
+                        else if (stringBuilder[stringBuilder.Length - 1] == str[j][0])
+                        {
+                            stringBuilder.Append(str[j][2]);
+                            table.Add(j);
+                            break;
+                        }
+                    }
+                }
+            }
+            return stringBuilder.ToString();
         }
         static string StringFromLetters(string[] arrString)
         {
